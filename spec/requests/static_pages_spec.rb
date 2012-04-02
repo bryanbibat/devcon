88bb32b63355e509815c4e0a906dcb3cb3a651c5
Terence Ponce <terenceponce@gmail.com>
2012-04-02 16:36:36 +0800

    Add home, contact, and about static page

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,35 @@
+require 'spec_helper'
+
+describe 'Static pages' do
+
+  subject { page }
+
+  shared_examples_for 'all static pages' do
+    it { should have_selector('h1', :text => heading) }
+    it { should have_selector('title', :text => full_title(page_title)) }
+  end
+
+  describe 'Home page' do
+    before { visit root_path }
+    let(:heading)    { 'Developers Connect' }
+    let(:page_title) { 'Home' }
+
+    it_should_behave_like 'all static pages'
+  end
+
+  describe 'Contact page' do
+    before { visit contact_path }
+    let(:heading)    { 'Contact' }
+    let(:page_title) { 'Contact' }
+
+    it_should_behave_like 'all static pages'
+  end
+
+  describe 'About page' do
+    before { visit about_path }
+    let(:heading)    { 'About' }
+    let(:page_title) { 'About' }
+
+    it_should_behave_like 'all static pages'
+  end
+end
