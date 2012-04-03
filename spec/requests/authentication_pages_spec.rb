906d5171d43944ef7d08347455660dfb6f3e7961
Terence Ponce <terenceponce@gmail.com>
2012-04-03 18:07:54 +0800

    Add integration tests for authentication

---
+++ spec/requests/authentication_pages_spec.rb
@@ -0,0 +1,48 @@
+require 'spec_helper'
+
+describe 'Authentication' do
+
+  subject { page }
+
+  describe 'signin page' do
+    before { visit new_user_session_path }
+
+    it { should have_selector('h1', :text => 'Sign in') }
+    it { should have_selector('title', :text => full_title('Sign in')) }
+  end
+
+  describe 'signin' do
+    before { visit new_user_session_path }
+
+    describe 'with invalid information' do
+      before { click_button 'Sign in' }
+
+      it { should have_selector('title', :text => 'Sign in') }
+      it { should have_error_message('Invalid') }
+
+      describe 'after visiting another page' do
+        before { click_link 'Home' }
+
+        it { should_not have_selector('div.alert.alert-error') }
+      end
+    end
+
+    describe 'with valid information' do
+      let(:user) { Fabricate(:user) }
+      before { capybara_signin(user) }
+
+      it { should have_selector('title', :text => full_title('')) }
+
+      it { should have_link(user.email, :href => '#') }
+      it { should have_link('Settings', :href => edit_user_registration_path) }
+      it { should have_link('Sign out', :href => destroy_user_session_path) }
+      it { should_not have_link('Sign in', :href => new_user_session_path) }
+
+      describe 'followed by signout' do
+        before { click_link 'Sign out' }
+
+        it { should have_link('Sign in') }
+      end
+    end
+  end
+end
