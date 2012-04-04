8d460e4539095707e01f3981eea506e3bf5e3756
Terence Ponce <terenceponce@gmail.com>
2012-04-04 13:35:01 +0800

    Customize devise routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,13 @@ Devcon::Application.routes.draw do
   devise_for :users
   resources :users
 
+  devise_scope :user do
+    get 'login', :to => 'devise/sessions#new', :as => :new_user_session
+    delete 'logout', :to => 'devise/sessions#destroy', :as => :destroy_user_session
+    get 'register', :to => 'devise/registrations#new', :as => :new_user_registration
+    get 'settings', :to => 'devise/registrations#edit', :as => :edit_user_registration
+  end
+
   match '/contact', :to => 'static_pages#contact'
   match '/about',   :to => 'static_pages#about'
 
