04b5fd570c794ad1a83ef335ef39c1c2430ed70e
Terence Ponce <terenceponce@gmail.com>
2012-04-09 16:37:51 +0800

    Annotate category model

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: categories
+#
+#  id          :integer         not null, primary key
+#  name        :string(255)
+#  description :text
+#  created_at  :datetime        not null
+#  updated_at  :datetime        not null
+#
+
 class Category < ActiveRecord::Base
   attr_accessible :description, :name
   has_and_belongs_to_many :articles
