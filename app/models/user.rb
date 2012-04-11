03f7cdab661fd8c0d23bb5844a98af5e20ad6aa7
Terence Ponce <terenceponce@gmail.com>
2012-04-11 15:23:15 +0800

    Update annotation of user

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,6 +16,7 @@
 #  created_at             :datetime        not null
 #  updated_at             :datetime        not null
 #  name                   :string(255)
+#  roles_mask             :integer
 #
 
 class User < ActiveRecord::Base
