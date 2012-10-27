004413ccf704f273bfe7cb21ec4f932eb048b618
Bryan Bibat <bry@bryanbibat.net>
2012-10-28 02:14:25 +0800

    Annotate CKEditor models

--- app/models/ckeditor/attachment_file.rb
+++ app/models/ckeditor/attachment_file.rb
@@ -5,3 +5,20 @@ class Ckeditor::AttachmentFile < Ckeditor::Asset
     @url_thumb ||= Ckeditor::Utils.filethumb(filename)
   end
 end
+# == Schema Information
+#
+# Table name: ckeditor_assets
+#
+#  id                :integer         not null, primary key
+#  data_file_name    :string(255)     not null
+#  data_content_type :string(255)
+#  data_file_size    :integer
+#  assetable_id      :integer
+#  assetable_type    :string(30)
+#  type              :string(30)
+#  width             :integer
+#  height            :integer
+#  created_at        :datetime        not null
+#  updated_at        :datetime        not null
+#
+
