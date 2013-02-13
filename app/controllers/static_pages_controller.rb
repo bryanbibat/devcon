fd1f292690c0f9bfd148c4fdcf1091b90c70cbce
Bryan Bibat <bry@bryanbibat.net>
2013-02-13 20:05:20 +0800

    Add basic RSS

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -7,4 +7,20 @@ class StaticPagesController < ApplicationController
 
   def about
   end
+
+  def feed
+    @title = "Developers Connect Philippines"
+    @entries = 
+      (Event.include_subevents.all + Article.all)
+      .sort_by(&:updated_at).reverse
+    @updated = unless @entries.empty?
+      @entries.first.updated_at
+    else
+      DateTime.now
+    end
+    respond_to do |format|
+      format.atom { render :layout => false }
+      format.rss { redirect_to feed_path(:format => :atom), :status => :moved_permanently }
+    end
+  end
 end
