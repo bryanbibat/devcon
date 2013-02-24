06107dda7f3057e87dad87107cb324ae31809e5a
Terence Ponce <terenceponce@gmail.com>
2013-02-24 16:40:19 +0800

    Remove tests on About/Sign up page. Use new title helper on Sign in page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,7 +5,6 @@ describe 'Static pages' do
   subject { page }
 
   shared_examples_for 'all static pages' do
-    it { should have_selector('h1', :text => heading) }
     it { should have_selector('title', :text => full_title(page_title)) }
   end
 
@@ -26,24 +25,12 @@ describe 'Static pages' do
     it_should_behave_like 'all static pages'
   end
 
-  describe 'About page' do
-    before { visit about_path }
-    let(:heading)    { 'About' }
-    let(:page_title) { 'About' }
-
-    it_should_behave_like 'all static pages'
-  end
-
   it 'should have the right links on the layout' do
     visit root_path
-    click_link 'About'
-    page.should have_selector('title', :text => full_title('About'))
     click_link 'Contact'
     page.should have_selector('title', :text => full_title('Contact'))
     click_link 'Home'
     page.should have_selector('title', :text => full_title(''))
-    click_link 'Sign up'
-    page.should have_selector('title', :text => full_title('Sign up'))
     click_link 'Sign in'
     page.should have_selector('title', :text => full_title('Sign in'))
     click_link 'Articles'
