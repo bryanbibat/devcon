63bf4efec902ab7a5c7a282531fd534b941e44b3
Bryan Bibat <bry@bryanbibat.net>
2013-03-16 03:23:46 +0800

    Temporarily use Azure CDN

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,4 +64,6 @@ Devcon::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+  config.asset_host = "http://az413909.vo.msecnd.net/devcon"
 end
