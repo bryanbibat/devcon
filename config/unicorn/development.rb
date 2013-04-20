c68f2c2de8dfe12f9d2e260178b6b467383c3291
Bryan Bibat <bry@bryanbibat.net>
2013-04-20 16:08:12 +0800

    Tweak Unicorn settings [ci skip]

--- config/unicorn/development.rb
+++ config/unicorn/development.rb
@@ -1,6 +1,6 @@
-APP_ROOT = File.expand_path(File.dirname(File.dirname(__FILE__)))
+APP_ROOT = File.expand_path(File.dirname(File.dirname(File.dirname(__FILE__))))
 
-ENV['BUNDLE_GEMFILE'] = File.expand_path('../Gemfile', File.dirname(__FILE__))
+ENV['BUNDLE_GEMFILE'] = File.expand_path('../../Gemfile', File.dirname(__FILE__))
 require 'bundler/setup'
 
 worker_processes 1
