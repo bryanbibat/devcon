e4a433a1e9b192024c397f69540e17ce8dd36901
Bryan Bibat <bry@bryanbibat.net>
2013-05-23 01:28:53 +0800

    Move to self-host assets [ci skip]

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,5 +65,5 @@ Devcon::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
-  config.asset_host = "http://az413909.vo.msecnd.net/devcon"
+  config.asset_host = "http://assets.devcon.ph"
 end
