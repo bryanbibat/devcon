eb16d7b5fec71a10612506a751d2daac09d48af8
Bryan Bibat <bry@bryanbibat.net>
2013-06-01 00:20:38 +0800

    Revert asset host [ci skip]

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,5 +65,5 @@ Devcon::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
-  #config.asset_host = "http://assets.devcon.ph"
+  config.asset_host = "http://assets.devcon.ph"
 end
