cc94bdc3d77f253566dc88078161b15c75b80b9f
Terence Ponce <terenceponce@gmail.com>
2013-07-27 12:56:17 +0800

    Add tests to completely cover Entity model

--- spec/models/entity_spec.rb
+++ spec/models/entity_spec.rb
@@ -20,4 +20,19 @@
 require 'spec_helper'
 
 describe Entity do
+  before { @entity = Fabricate.build(:entity) }
+  subject { @entity }
+
+  it { should validate_presence_of(:type) }
+  it { should ensure_inclusion_of(:type).in_array(["school", "company", "community"]) }
+  it { should ensure_inclusion_of(:level).in_array(["cool", "awesome"]) }
+
+  it { should have_many(:event_partners) }
+  it { should have_many(:events).through(:event_partners) }
+
+  describe "#inheritance_column" do
+    it "must disable STI to use 'type' as a column name" do
+      Entity.inheritance_column.should eq("disabled")
+    end
+  end
 end
