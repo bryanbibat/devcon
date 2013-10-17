8b024d3026a517c28bae0e02e497b0a01189b6c6
Bryan Bibat <bry@bryanbibat.net>
2013-10-18 04:37:58 +0800

    Test removing admin.js

--- config/application.rb
+++ config/application.rb
@@ -57,7 +57,7 @@ module Devcon
     # Prevents access to DB when precompiling
     config.assets.initialize_on_precompile = false
 
-    config.assets.precompile += ['admin.js']
+    # config.assets.precompile += ['admin.js']
 
     config.generators do |g|
       g.test_framework :rspec, :fixture => true
