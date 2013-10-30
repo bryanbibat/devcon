fa6ffc24b502adee1dbf4c2c94dd424417a02e3b
Bryan Bibat <bry@bryanbibat.net>
2013-10-31 07:56:09 +0800

    Fix ckeditor workaround

--- config/deploy.rb
+++ config/deploy.rb
@@ -38,22 +38,6 @@ after "deploy:update", "deploy:cleanup", "deploy:migrate"
 
 require "capistrano-unicorn"
 
-namespace :ckeditor do
-  def copy_assets(regexp)
-    Rails.application.assets.each_logical_path(regexp) do |name, path|
-      asset = Rails.root.join('public', 'assets', name)
-      p "Copy #{path} to #{asset}"
-      FileUtils.cp path, asset
-    end
-  end
-
-  desc 'Copy ckeditor assets, that cant be used with digest'
-  task copy_nondigest_assets: :environment do
-    copy_assets /ckeditor\/contents.css/
-    copy_assets /ckeditor\/skins\/moono\/.+png/
-  end
-end
-
 desc 'copy ckeditor nondigest assets'
 task :copy_nondigest_assets, roles: :app do
   run "cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} ckeditor:copy_nondigest_assets"
