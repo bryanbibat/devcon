d0bc875b231f2d6da49769e7d2f8d2e24bb57f64
Bryan Bibat <bry@bryanbibat.net>
2013-10-31 10:04:12 +0800

    Remote compilation doesn't work, settle for local compilation and disabling of cleaning old assets

--- config/deploy.rb
+++ config/deploy.rb
@@ -44,3 +44,11 @@ task :copy_nondigest_assets, roles: :app do
   run "cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} ckeditor:copy_nondigest_assets"
 end
 after 'deploy:assets:precompile', 'copy_nondigest_assets'
+
+namespace :deploy do
+  namespace :assets do
+    task :clean_expired, :roles => lambda { assets_role }, :except => { :no_release => true } do
+      run "cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} assets:clean"
+    end
+  end
+end
