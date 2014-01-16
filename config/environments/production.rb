2643a7a93c9c881fceba2b29427aa367a6aa649c
Bryan Bibat <bry@bryanbibat.net>
2014-01-17 02:15:29 +0800

    Adjust asset host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,7 +48,7 @@ Devcon::Application.configure do
   # config.cache_store = :mem_cache_store
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
-  # config.action_controller.asset_host = "http://assets.example.com"
+  config.action_controller.asset_host = "http://assets.devcon.ph"
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,3 +1,3 @@
 CarrierWave.configure do |config|
-#  config.asset_host = "http://assets.devcon.ph"
+  config.asset_host = "http://assets.devcon.ph"
 end
